befb96faded516ceb9bfac412df8470d3c144a91
Volodymyr Lyubinets <vlyubin@gmail.com>
2016-06-11 16:08:12 -0700

    hackz

--- Website/static/main.js
+++ Website/static/main.js
@@ -3,11 +3,13 @@
 var curState = "";
 var acceptInput = 1;
 
-var tts_key = "EBJ4mGEpKmzU9vGAAxms0MOy2mJKRL5dEfJsIr81sEYpUfcYPTPWHdLephdl91aRvBfxuGZYhSKXO%2B%2BDUi%2BbYwfs%2BpESYSieSO1sIOl%2BJMFfjzdcl2fbUdH4Ib5W6A7k1DheFVwz3vIi%2Fl4ieOblbxHqV7g%2FO6FFBQ8DwOShEeCzSoKcA4TEt06cVvZJGYSI1h6waeebq0MxwMqjaRsCUgGAId%2Fg4taNAi%2BBgjHJ3d%2Fw%2BA%2BFcCYr6FW64Bixz5Aao73mbfSb75Q89hVObGeiZyuoiHLdktsR0%2BRma1LHuBEaDh0x1SjBD1ZIJun36VLEKeb%2BdmuJNXNmoV0GVCi0J66BPLUSg%2BGKX1iJ8kmO2bmvTIntY55uNvKdIWlN8MgFmpVQizJS6jorHL1PFzY4qqtN6Bq6LNqCI992rgCk3ptM5jQ9ajLmRcaIsSL8E5nxcrK2o7Eallpv0WKHv9gWVZCtsZTStvVa50OVreGbAoN4GkzP3XH7jsLZvCHWrcIgX244eIdgLnWwc6wmNizcxnePH2B2QikYk1mrEeju5e9DTqONpIDzd7FtEYxsmFkJj6kAbgscv6ZKGlt9u7UIEmh6nQ8uICVhse4R7I5MjaXaVw123Os12MA9KnwNpps52Edm88Oyh5jsP98jz7RP6es%2FJr5blU4vEBRf6W9nFGj4OL8g%2FgkLT9EagtUryhAxbmrUVrYhGrnQqWCk3mGuZF00fZ%2F5oooTP4GdStCoWyRo8qOT3YpPi2ch%2BwckZP6j%2FXCFlYI65JLgLLNH2XFFXErjSYEaQiVZ%2FDZJjLF1eqdA64rnnjoPWaKWloIJ6vA5jN9FVLwVCCEO8N3m29MobACzYIJj%2Fw8j";
-var stt_key = "pEpJZ8Nse0YolMn70B23jKxY4V61LiBr1cawXB6MSBiaNo%2Bl4V2Gz8BJK9%2F%2BP6fxSZr5F01wIx6B0SmbvH%2Bdf8ZX4MZ1n0e%2F5pNItam47ZFJEgzUEpblWxh25PRKIgdg5Q9B1DCZDnaHik21%2BahBqPbk4PgNlMQfWk3PTAtZpeQKezNolfdXHwdZKe%2BM8%2BllvjMoKfKqgQ9tR1ToMmGyUETQwiJDexHTGF3kFzsbB7H6LQ%2FsKlMTn14vYXXjgNewNk9Kd41dXAgSCH07Ef8505nmwchwte9gVlh2PsXtaUkTfikzom0Q5puzbwz308xZ3vzjZMiTJxm%2FVE8lCEWTvqI020jW8IG3OA8sTE%2F1LfIEdV60oi3HikT2CYQ%2BFdnyxKz6Kz3lJRyTiDmCWIy5g8%2FXV2GfneXS5BgULs0Ciem61xXEvhFpePXfM7bbne%2BUlnCZc6IohaDS3xM24e5IHgaL5FZ6oAiEh64MZ73eNKiikk4xEzti2zvc8a9U9oSk8I6r%2FOy4xDxlfoksGFxYfoKirTBpTU6tMUcpadDBMu%2F%2B48Dp0K2iWxk2u1%2FUwH0NUyP1lHB8w0MNZXDyphW%2FEj4MQmZy%2B%2BOaOU2i%2Blejw5UQOtkP4mc5y3JDGNNEOphPKbb%2FG0S9AXJ2r0F89Szlawnigux1V%2Flb9G18C6Tek42l9kqZdlhvBoipy8QdwL56RsQav%2BxPc2Es4r8%2B2qm%2FOIO53ehyYzcV%2Bdlz8XpaaeurpKweGKick7twBxIcfMiVJrIQr2QgNzExb552guNK3p4l2dltKzEieNVlZkVWv3pRlmXePcBEPuQg48170tyfUPvKmIW851Cu%2FrMbpSGL%2B5Ba%2FfPXgyuT";
+var tts_key = "sqaAbecYYi%2BULx3nKlrY%2BWuE2UUWh8KyoI9fIQks6yQsCrsnlDqdeWgJHB0Ug%2BUFhC9tEj%2BNpTjGO3iM2U%2FDo5rQ%2FbQ7WylRbPc42CxwdYgoY2SmObOykWkWr3Gd1G6BF9UVtyHHUtt2epJBvTN7NQjjwmUS67pIegQT82EOxW4SHTY1w92JkF7JP%2BcczYqNW5lFwGid6Z3QsI21Y85UkFV3fYkVAX1ocbOsgqSsuld1zTxvMXzLfxBmVRku%2BU65xT2Ehw6j2XOr4FtkR0LlJQG05LB%2Bc1yMeVTIdxpYXNuriURJ6h%2F4yZR3JcmbqvxtDDSuLWqSuv5tQ9OORH3tjeevwWFY2CKH0RUgb145f5AXKyHKbfDVgUOHtqSajYAiCtFcvXIzur%2Fj73E7bksLh8X78aSKFeHhc2Zoxky1GMmC0zGMKaD89Z%2BcjWQnULDbjnIFoi0OphwvKVbqDIcnhRq92iA5RLnHdmKXIiI9xG%2FofwZc8l5VHFD2RFdUWMNIKxZBAPAHfZrhm4ZgVFWhPnFmVNcS17ZMndrQme3kXLUezMokyBJNHLb4TOog0HHPGzrb49FBhKkTjqRAJc5Ftw7hM7jrDH06DjLctdszBu1QfjYB7bFf0ZjdY2KSSmt1wbuWCQi31oauIxpAr3VI3Jl%2FnL1KXNQd5DzFMMy98QUD2cTElHzh1Uoy7X1V3RNSKtyTU8u2PBI%2FhvBvhaEOXAbV9GOJhF6ojryqiCwB%2Fpmr4%2BMchZE0pdpPjbNkvXBDq0oX2UOBmuAWpOJnFlJfqDMCtJkvQAlcGg%2Fgf%2FUjJDI7xcb1E0RenSn0f6EcPFifiVzBrHZpQn%2BkUvqt%2BHapql34I%2F2xpClP";
+var stt_key = "%2FvfQ%2F6OQUy5rlfFhYrLvTu5%2FxPs24NPfQyy3IutHptqNlT2PTO1WAKKfzBx9qrdETAtxPFE0h1hfMufdFSd24lTFcdJhnciRlyP04j6CrDrom7xpQg01dk0arTL2D1ZsqhXD%2FgKAWFieKWg0e%2BDnpR%2FLi6%2FxgBzUKPIBu%2By1J%2BYtWRRFtFwY%2F%2FxcH1mPHTOBx2xGBAl6LjxracG4z0IW7unU7ROb8MtVMJAiDVUdhABzUyU6c9P%2FS7w%2FM77jOJdeH68GEf4rXhDHrRpA96pVMOpsCnZkA65RwId9Gww0oOsVKYLXMfdRpb7VeKlwusgziqxFTOl3yYdGrUmb4ycCgWgZsPAXRpqiKBfd8Dfti4HA%2BFC45kgXen1a1ELpbydZPtLqSM2DjDSsIM4gBLKuANHXt4u2zHQNgKvgg5yUidyC5BbuubDV1vy%2FDv7FTldbmm4KbQz1L11zH9YGLXzue7YVYfVw%2FDdBRcc17KDOK%2F6VO5pz4k2hrN4QbI1WppMkve%2B9fE%2FKp2DTEVH2abNfKC0FUPYn0a3jBsZskB9yiDUM%2BGJS4Zkbs16V7V2pyR2dcd3S13VAxSDkNW183YpqW95ketdFPRQAsa6CZtUQxDfJjD1Y3MBBU1dM8kllD9Pm%2FphvnwjdLyHOTVS4U33HP1G1EgKq0Efik4OB%2Fy66v50%2B3OUW%2FXRIF%2FJDjZvuP7eTWTPUWNFH4uxeeruC8fXZTjlQLomz8aDIL621yp%2BcozEYHU8Vdyfn0z58V0Q7bc7Pwd16YwkFINUZJbytqgotCu6OEOlJuDeva%2B3LB8PzCxxdVxHmie3CvfsvPn6wtiFFoJBL%2Bv9xFESSuDR0BGgENV%2BZv%2BhWv7nd";
 
 var stream_loaded = false;
 var audio;
+
+var failed = 0;
 
 function transition_to_salad() {
   // First, fade the current page to the left
@@ -115,6 +117,7 @@ $("#mic-button").click(function () {
       // Walk through the array of key words and see if any one was found
       for (var i = 0; i < keywords.length && !found; ++i) {
         if (data.indexOf(keywords[i]) >= 0) {
+          found = true;
           var nextState = commands[k].goto;
 
           // Respond. Upon completion, apply the state transition.
@@ -124,6 +127,24 @@ $("#mic-button").click(function () {
 
           break;
         }
+      }
+
+      if (failed == 2) {
+        failed = 0;
+        found = true;
+        var nextState = commands[k].goto;
+
+        // Respond. Upon completion, apply the state transition.
+        watsonSay(commands[k].response, function() {
+          setState(nextState);
+        });
+      } else
+      if (!found) {
+        stream.pause();
+        watsonSay("Sorry, I didn't quite catch that!", function() {
+          stream.resume();
+        });
+        failed += 1;
       }
     }
   });
